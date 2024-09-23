26106962218c08a47e8fbedd9dfc53fcb89e6b2d
joselinares97 <linaresjosedavid@gmail.com>
2024-09-22 21:50:19 -0500

    Actualizacion credenciales para Railway

--- multielectricos/settings.py
+++ multielectricos/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'ZOQLUosmmzHjaqZzlHqoBuelzutBFVRU',
+        'PASSWORD': 'LEIzeaTRoIAgyNHfaduGtaXrYBmbPBaT',
         'HOST': 'mysql.railway.internal',
         'PORT': '3306',
     }
